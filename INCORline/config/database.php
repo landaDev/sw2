8a38db98f08030237dde8308e21433aeb09b5273
landaDev <landaqp.dev@gmail.com>
2021-03-16 13:20:29 -0400

    db changes

--- INCORline/config/database.php
+++ INCORline/config/database.php
@@ -91,6 +91,29 @@ return [
             'prefix_indexes' => true,
         ],
 
+        // 'mysql' => [
+        //     'driver' => 'mysql',
+        //     'host' => 'db4free.net',
+        //     'port' => '3306',
+        //     'database' => 'incorline',
+        //     'username' => 'pandalandia',
+        //     'password' =>'pandalandia',
+        //     'charset' => 'utf8mb4',
+        //     'collation' => 'utf8mb4_unicode_ci',
+        //     'prefix' => '',
+        //     'strict' => false,
+        //     'engine' => null,
+        //     'modes'=>[
+        //     'ONLY_FULL_GROUP_BY',
+        //     'STRICT_TRANS_TABLES',
+        //     'NO_ZERO_IN_DATE',
+        //     'NO_ZERO_DATE',
+        //     'ERROR_FOR_DIVISION_BY_ZERO',
+        //     'NO_ENGINE_SUBSTITUTION',
+        //     ],
+            
+        // ],
+
     ],
 
     /*
